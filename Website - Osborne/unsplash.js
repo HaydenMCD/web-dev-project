659da52b65d1a20ed528c16d9817cd67a049aa05
Hayden <mcdohr2@student.op.ac.nz>
2020-10-26 17:54:29 +1300

    Fixed bug with home links. Chaanged client span

---
+++ Website - Osborne/unsplash.js
@@ -0,0 +1,10 @@
+//Access key: 8BjrXDzcvxDXgcdlDo7mgCAJHKP3BLDgKTlrWAwFsO8
+//Private key: ZSQ4zxpRw7heN3bem88ObSVsJlfSgrkaKuvVtkXny6s
+
+
+const api = 'https://api.unsplash.com/photos/?8BjrXDzcvxDXgcdlDo7mgCAJHKP3BLDgKTlrWAwFsO8';
+
+
+var getImg = new XMLHttpRequest();
+var image = document.getElementById("image");
+
