6c6102de0765f90a08f1dffb3544a031ba890ead
Hayden McDowall (1000051359) <MCDOHR2@student.op.ac.nz>
2020-10-30 14:57:43 +1300

    Added two more clients. removed fourth page height

--- Website - Osborne/Menu.js
+++ Website - Osborne/Menu.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 let menuOpen = false;
+
+
 menuBtn.addEventListener('click', () => {
     if (!menuOpen){
         menuBtn.classList.add('open');
