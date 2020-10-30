2ba0e44e4fc0f4981eb529d04a5b8e779953d7ba
Hayden <MCDOHR2@student.op.ac.nz>
2020-10-30 13:24:15 +1300

    Played with unsplash

--- Website - Osborne/unsplash.js
+++ Website - Osborne/unsplash.js
@@ -2,6 +2,19 @@
 //Private key: ZSQ4zxpRw7heN3bem88ObSVsJlfSgrkaKuvVtkXny6s
 
 
-const api = 'https://api.unsplash.com/photos/?8BjrXDzcvxDXgcdlDo7mgCAJHKP3BLDgKTlrWAwFsO8';
+const api = 'https://api.unsplash.com/photos/?client_id=?8BjrXDzcvxDXgcdlDo7mgCAJHKP3BLDgKTlrWAwFsO8';
 
 
+//const audi = 'https://unsplash.com/photos/XqMiUqByyHA'
+//const BMW = 'https://unsplash.com/photos/17ibDRPPszE'
+//const focus = 'https://unsplash.com/photos/t1_slwbaBp8'
+
+//const audiDriver= 'https://unsplash.com/photos/ILip77SbmOE'
+//const BWMDriver= 'https://unsplash.com/photos/rriAI0nhcbc'
+//const focusDriver= 'https://unsplash.com/photos/VVEwJJRRHgk'
+
+
+
+
+
+let pic = fetch ('https://api.unsplash.com/search/photos?query=race_car&client_id=9XxUx99pLLvdMrq1H2aJOedovrSUDNfc_GfTQIz0_zY')
