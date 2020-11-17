420d49c109509e1dc004943898ef6771afd66881
Hayden McDowall (1000051359) <MCDOHR2@student.op.ac.nz>
2020-11-18 10:04:34 +1300

    Changed Quote API but sitll not working

--- knobody/files/JS/QuoteAPI.js
+++ knobody/files/JS/QuoteAPI.js
@@ -5,9 +5,36 @@ fetch('http://localhost:3000/quotes')
 .then(function(data){
     console.log(data);
     let quotes = data;
-    addtohtml(quotes);
+    genQuotes(quotes);
 });
 
-function addtohtml(quotes){
-    $('#quote1').append(quotes[0].test);
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function genQuotes(quotes){
+
+    var quoteNumbers = [];
+
+    for (let i = 0; i < 8; i++)
+    {
+        quoteNumbers.push(randomInt(0,52));
+    }
+
+    var noDouble = quoteNumbers.reduce(function(a,b){
+        if (a.indexOf(b) < 0) a.push(b)
+        {
+            return 0;
+        }
+    },[]);
+
+    $('#quote1').append(quotes[noDouble[0]].text);
+    $('#quote2').append(quotes[noDouble[1]].text);
+    $('#quote3').append(quotes[noDouble[2]].text);
+    $('#quote4').append(quotes[noDouble[3]].text);
+    $('#quote5').append(quotes[noDouble[4]].text);
+    $('#quote6').append(quotes[noDouble[5]].text);
+    $('#quote7').append(quotes[noDouble[6]].text);
+    $('#quote8').append(quotes[noDouble[7]].text);
 }
